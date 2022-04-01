0cbbf5a36260011edb20ba0965e049a11d0c2311
JDGM01 <yodaz95@gmail.com>
2022-04-01 05:57:21 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-25xs5a
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-25xs5a)
---
+++ src/app/product-list/product-list.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+import { products } from '../products';
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css'],
+})
+export class ProductListComponent {
+  products = products;
+
+  share() {
+    return alert('The product has been shared!');
+  }
+}
